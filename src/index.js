960fa465a1df37d2242ce122cfab95dc7b5af529
Katheer Mizal <kmizal.c@rex.com>
2023-03-18 17:53:07 +0530

    Update the tailwind.css file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import './styles/tailwind.css';
+
 import App from './App';
+
+import './styles/tailwind.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
